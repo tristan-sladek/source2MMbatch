803d4f74375da6409447d48f43c78e5d31c31fe0
Tristan Sladek <tristansladek@gmail.com>
2021-07-30 18:48:30 -0500

    Initialized VS

---
+++ source2MMBatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace source2MMBatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
